ddc12379449eb1b9e74ad06173d40d643f2d78d3
johnzoki <main@login.jh-bergmann.de>
2024-05-28 12:01:55 +0200

    get_comp_name

--- src/archicad2elca_comps/ifc_extractor.py
+++ src/archicad2elca_comps/ifc_extractor.py
@@ -22,9 +22,9 @@ class IfcLayer:
 def multi_layer(ifc_rel_aggregates):
 	first_aggregate = ifc_rel_aggregates[0]
 	element_comp = first_aggregate[5]
-	for ifc_element_part in element_comp:
-		print(ifc_element_part)
+	for layer_number, ifc_element_part in enumerate(element_comp, 1):
 		ifc_element_part_name = ifc_element_part[2]
+		print(f"Layernumber: {layer_number}")
 		print(f"Name of Element_Part: {ifc_element_part_name}")
 		for i in ifc_element_part.IsDefinedBy:
 			if i.is_a('IfcRelDefinesByProperties') and i.RelatingPropertyDefinition.is_a('IfcElementQuantity'):
@@ -55,10 +55,17 @@ def get_name_and_width(m, ifc_element):
 	element_ref = m.get_inverse(ifc_element)
 	ifc_rel_aggregates = [rel for rel in element_ref if rel.is_a("IfcRelAggregates")]
 	if ifc_rel_aggregates:
+		psets = ifcopenshell.util.element.get_psets(ifc_element)
+		if psets.get('ArchiCADProperties'):
+			if psets.get('ArchiCADProperties').get('Mehrschichtige Bauteile'):
+				print(psets.get('ArchiCADProperties').get('Mehrschichtige Bauteile'))
 		multi_layer(ifc_rel_aggregates)
 	else:
 		print("No IfcRelAggregates found for Wall")
 		wall_single_layer(ifc_element)
+
+def get_guid(ifc_element):
+	return
 
 def run():
 	m = ifcopenshell.open("ifc_import/TestIFC.ifc")
